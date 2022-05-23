877ab4df83a0cf3653615689c08fe02860120479
nguyenquocthien <nguyenquocthien810@gmail.com>
2022-05-23 16:04:21 +0700

    adding upload functionality, product model & storing products in the database

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product.model');
+
 const getProduct = (req, res) => {
   res.render('admin/products/all-products');
 }
@@ -6,8 +8,22 @@ const getNewProduct = (req, res) => {
   res.render('admin/products/new-product');
 }
 
-const createNewProduct = (req, res) => {
-  
+const createNewProduct = async (req, res, next) => {
+  // console.log(req.file);
+  // console.log(req.body);
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename
+  });
+
+  try {
+    await product.save();
+  } catch (err) {
+    next(err);
+    return;
+  }
+
+  res.redirect('/admin/products');
 }
 
 module.exports = {
---
+++ middlewares/image-upload.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+const uuid = require('uuid').v4;
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'product-data/images');
+    },
+    filename: (req, file, cb) => {
+      cb(null, uuid() + '-' + file.originalname);
+    },
+  }),
+});
+
+const configuredMulterMiddleware = upload.single('image');
+
+module.exports = configuredMulterMiddleware;
